80f01f11e6fb4a0362a54ff134db0a483cc2c42c
Durga Vara Praveen Geda <durgavarapraveen@gmail.com>
2023-08-07 20:32:49 +0530

    Responsive for all web pages

--- Gallery.js
+++ Gallery.js
@@ -1,35 +1,17 @@
-function gallerydropdown() {
-    console.log("Entered into function")
-    document.getElementById('dropdown').classList.toggle("show");
-}
-window.onclick = function(event) {
-    if(!event.target.matches('dropdown-button')) {
-        var dropdowns = document.getElementsByClassName('gallery-dropdown');
-        var i =0;
-        console.log(dropdowns);
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-              openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
   
   // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
+window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
         }
-      }
     }
   }
