1166d5c4d9e00d749dacd2c043b417f992cc17d2
Durga Vara Praveen Geda <durgavarapraveen@gmail.com>
2023-09-12 12:14:22 +0530

    home page responsive

--- Home.js
+++ Home.js
@@ -9,6 +9,7 @@ gsap.to('.approval-name-1', {
         trigger: ".approval-name-2",
         start: "top center",
         end: "bottom 40%",
+        marker: true,
         toggleActions: "restart reverse restart reverse",
     }
 });
@@ -25,10 +26,11 @@ gsap.to('.approval-name-3', {
     }
 });
 
+
 const timeline = gsap.timeline({
     scrollTrigger: {
         trigger: ".flip-card-1",
-        start: "top 60%",
+        start: "top 80%",
         end: "bottom 30%",
         // markers: true,
         toggleActions: "restart reverse restart reverse"
@@ -36,19 +38,19 @@ const timeline = gsap.timeline({
 });
 
 // Add the first animation to move the image horizontally
-timeline.to('.gsap-img-1', {
-    x: "125%",
-    duration: 0.4,
-    ease: 'power2.out'
-});
+// timeline.to('.gsap-img-1', {
+//     x: "125%",
+//     duration: 0.4,
+//     ease: 'power2.out'
+// });
 
 
 // Add the second animation to make the image disappear
-timeline.to('.gsap-img-1', {
-    scale: 0,
-    duration: 0.2,
-    ease: 'power2.out'
-});
+// timeline.to('.gsap-img-1', {
+//     scale: 0,
+//     duration: 0.2,
+//     ease: 'power2.out'
+// });
 
 timeline.to('.flip-card-1', {
     opacity: 1,
@@ -57,29 +59,31 @@ timeline.to('.flip-card-1', {
 
 const timeline2 = gsap.timeline({
     scrollTrigger: {
-        trigger: ".flip-card-1",
-        start: "top 60%",
+        trigger: ".flip-card-2",
+        start: "top 80%",
         end: "bottom 30%",
         // markers: true,
         toggleActions: "restart reverse restart reverse"
     }
 });
 
-timeline2.to('.gsap-img-2', {
-    x: "-125%",
-    duration: 0.4,
-    ease: 'power2.out'
-});
+// timeline2.to('.gsap-img-2', {
+//     x: "-125%",
+//     duration: 0.4,
+//     ease: 'power2.out'
+// });
 
 
 // Add the second animation to make the image disappear
-timeline2.to('.gsap-img-2', {
-    scale: 0,
-    duration: 0.2,
-    ease: 'power2.out'
-});
+// timeline2.to('.gsap-img-2', {
+//     scale: 0,
+//     duration: 0.2,
+//     ease: 'power2.out'
+// });
 
 timeline2.to(".flip-card-2", {
     opacity: 1,
     duration: 0.2,
 });
+
+
