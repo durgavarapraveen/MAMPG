f4513ae96a7ac0290cc2f94beaeb90a7414a694e
Durga Vara Praveen Geda <durgavarapraveen@gmail.com>
2024-01-22 12:56:59 +0530

    Added backend

--- Admin/Commitee-admin.js
+++ Admin/Commitee-admin.js
@@ -4,10 +4,44 @@ const ListtextChange = [];
 const idRowChange = [];
 const ListRowChnage = [];
 const IdDeleteButtonds = [];
+const addedPoints = [];
 
 const paragraph = document.querySelectorAll('p')
 const textTable = document.querySelectorAll('td');
 const saveButton = document.getElementById('saveButton')
+const textInput = document.querySelectorAll('.input')
+const submitBtn = document.querySelectorAll('.input-btn')
+const form = document.getElementsByClassName('.form')
+const left = document.querySelectorAll('.left');
+
+for (var i = 0; i < form.length; i++) {
+    form[i].addEventListener('click', function(event) {
+      event.preventDefault();
+    });
+}
+
+textInput.forEach((text, index) => {
+    text.addEventListener('click', function() {
+        const lefts = left[index]
+        const input = this.previosElementSibling;
+        const point = input.value;
+        if(point!=='') {
+            const listItem = document.createElement('p');
+            listItem.innerHTML = `${point} <button  id="cac-1" onclick="DeleteBtn(this)" class="btn-icon"><i class="fa-solid fa-trash"></i></button>`;
+            addedPoints.push(listItem);
+            lefts.appendChild(listItem);
+        }
+        const deleteButton = listItem.querySelector('.delete-button');
+        deleteButton.addEventListener('click', function() {
+                    // Remove the clicked list item and remove it from the array
+        pointList.removeChild(listItem);
+            addedPoints.splice(addedPoints.indexOf(listItem), 1);
+        });
+
+                // Clear the text area
+        input.value = '';
+    })
+})
 
 
 paragraph.forEach((text) => {
@@ -33,6 +67,7 @@ textTable.forEach((text) => {
 
 })
 
+
 function DeleteBtn(element) {
     console.log('Clicked')
     const Id = element.id;
@@ -43,6 +78,7 @@ function DeleteBtn(element) {
     X.style.display = 'none';
     console.log(IdDeleteButtonds)
 }
+
 
 saveButton.addEventListener('click', function() {
     const form = [{
